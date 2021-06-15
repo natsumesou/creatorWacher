5ac388d5d2a40eec808f2b4d9ade2f805ed2e93f
natsumesou <natsumesouxx@gmail.com>
2021-06-15 13:30:25 +0900

    更新

--- functions/src/fetchSuperChats.ts
+++ functions/src/fetchSuperChats.ts
@@ -1,32 +1,83 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import {findChatMessages, SuperChat} from "./lib/youtubeChatFinder";
 
 export const fetchSuperChats = async () => {
   const db = admin.firestore();
-  const channel = await db.collection("channels").doc("UCS9uQI-jC3DE0L4IpXyvr6w").get().catch((err) => {
+  const channels = await db.collection("channels").where("category", "==", "hololive").get().catch((err) => {
     functions.logger.error(err.message + "\n" + err.stack);
   });
 
-  if (!channel || channel && !channel.exists) {
+  if (!channels || channels && channels.empty) {
     return;
   }
 
-  const streams = await channel.ref.collection("streams").get().catch((err) => {
-    functions.logger.error(err.message + "\n" + err.stack);
+  const tempChannels: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
+
+  channels.forEach((channel) => {
+    tempChannels.push(channel);
   });
 
-  if (!streams || streams && streams.empty) {
-    return;
+  let count = 0;
+
+  for (const channel of tempChannels) {
+    const streams = await channel.ref.collection("streams").get().catch((err) => {
+      functions.logger.error(err.message + "\n" + err.stack);
+    });
+
+    if (!streams || streams && streams.empty) {
+      return;
+    }
+
+    const tempStreams: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
+    streams.forEach((stream) => {
+      tempStreams.push(stream);
+    });
+
+    for (const stream of tempStreams) {
+      const superChat = await stream.ref.collection("superChats").limit(1).get();
+      if (superChat.size !== stream.get("superChatCount")) {
+        functions.logger.info(`update superchats: ${channel.id}/streams/${stream.id}`);
+        const result = await findChatMessages(stream.id, stream.get("streamLengthSec"));
+        if (result.stream.chatAvailable) {
+          await saveSuperChats(channel.id, stream.id, result.superChats);
+        }
+        count += 1;
+      }
+      if (count === 5) {
+        break;
+      }
+    }
+    functions.logger.info(`----- updated ${count} streams`);
+    if (count === 5) {
+      break;
+    }
   }
+};
 
-  const tempStreams: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
-  streams.forEach((stream) => {
-    tempStreams.push(stream);
-  });
-
-  for (const stream of tempStreams) {
-    const superChat = await stream.ref.collection("superChats").limit(1).get();
-
-    functions.logger.info(stream.get("title") + " : " + stream.id + " : " + superChat.size);
+const saveSuperChats = async (channelId: string, videoId: string, superChats: {[id:string]: SuperChat}) => {
+  const db = admin.firestore();
+  let batch = db.batch();
+  const limit = 500;
+  let i = 0;
+  for (const id in superChats) {
+    if (!Object.prototype.hasOwnProperty.call(superChats, id)) {
+      continue;
+    }
+    const doc = db.collection(`channels/${channelId}/streams/${videoId}/superChats`).doc(id);
+    batch.set(doc, superChats[id]);
+    i += 1;
+    if (i === limit) {
+      await batch.commit().catch((err) => {
+        functions.logger.error(err.message + "\n" + err.stack);
+      });
+      batch = db.batch();
+      i = 0;
+    }
+  }
+  if (i !== limit) {
+    await batch.commit().catch((err) => {
+      functions.logger.error(err.message + "\n" + err.stack);
+    });
   }
 };
