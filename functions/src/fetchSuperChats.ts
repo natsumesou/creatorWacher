8264d97ae1792e6b263f83b367fb8a54c19e1f05
natsumesou <natsumesouxx@gmail.com>
2021-06-15 12:57:42 +0900

    test

--- functions/src/fetchSuperChats.ts
+++ functions/src/fetchSuperChats.ts
@@ -3,42 +3,29 @@ import * as admin from "firebase-admin";
 
 export const fetchSuperChats = async () => {
   const db = admin.firestore();
-  const channels = await db.collection("channels").where("category", "==", "hololive").get().catch((err) => {
+  const channel = await db.collection("channels").doc("UCS9uQI-jC3DE0L4IpXyvr6w").get().catch((err) => {
     functions.logger.error(err.message + "\n" + err.stack);
   });
 
-  if (!channels || channels && channels.empty) {
+  if (!channel || channel && channel.exists) {
     return;
   }
 
-  const tempChannels: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
-
-  channels.forEach((channel) => {
-    tempChannels.push(channel);
+  const streams = await channel.ref.collection("streams").get().catch((err) => {
+    functions.logger.error(err.message + "\n" + err.stack);
   });
 
-  for (const channel of tempChannels) {
-    const streams = await channel.ref.collection("streams").get().catch((err) => {
-      functions.logger.error(err.message + "\n" + err.stack);
-    });
+  if (!streams || streams && streams.empty) {
+    return;
+  }
 
-    if (!streams || streams && streams.empty) {
-      return;
-    }
+  const tempStreams: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
+  streams.forEach((stream) => {
+    tempStreams.push(stream);
+  });
 
-    const tempStreams: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
-    streams.forEach((stream) => {
-      tempStreams.push(stream);
-    });
-
-    for (const stream of tempStreams) {
-      functions.logger.info(channel.get("title") + ":" + stream.get("superChats"));
-      functions.logger.info(typeof stream.get("superChats"));
-      break;
-      // if () {
-      //   // await publishAnalyzeStream(doc.id, channel.id);
-      // }
-    }
-    break;
+  for (const stream of tempStreams) {
+    functions.logger.info(channel.get("title") + ":" + stream.get("superChats"));
+    functions.logger.info(typeof channel.get("superChats"));
   }
 };
