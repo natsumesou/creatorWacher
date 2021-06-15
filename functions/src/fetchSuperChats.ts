282401da8c5fc982eee85169e7e41169144fef7e
natsumesou <natsumesouxx@gmail.com>
2021-06-16 05:09:10 +0900

    check

--- functions/src/fetchSuperChats.ts
+++ functions/src/fetchSuperChats.ts
@@ -41,6 +41,7 @@ export const fetchSuperChats = async () => {
     for (const stream of tempStreams) {
       const superChat = await stream.ref.collection("superChats").limit(1).get();
       if (!stream.get("chatDisabled") && (superChat.size === 0 && stream.get("superChatCount") !== 0)) {
+        functions.logger.info(`------ publish ${channel.id}/streams/${stream.id} / ${!stream.get("chatDisabled") ? "true" : "false"} / ${superChat.size} / ${stream.get("superChatCount")}`);
         const pubsub = new PubSub({projectId: process.env.GCP_PROJECT});
         const topic = await pubsub.topic(TEMP_ANALYZE_TOPIC);
         const obj = {
