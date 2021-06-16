3ab188fda666839384c9dc95e28e4a8bcb98583f
natsumesou <natsumesouxx@gmail.com>
2021-06-17 07:00:13 +0900

    migration test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ import {analyzeChats} from "./analyzeChats";
 import {updateArchives} from "./updateArchives";
 import {RuntimeOptions} from "firebase-functions";
 import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
-import {migrateToBigQuery} from "./migrateToBigQuery";
+import {migrateStreams, migrateSuperChats, triggerSuperChats} from "./migrateToBigQuery";
 
 admin.initializeApp();
 
@@ -29,4 +29,6 @@ export const AnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(
 export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
 export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
 
-export const MigrateToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 5 minutes").onRun(migrateToBigQuery);
+export const MigrateStreams = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.schedule("every 10 minutes").onRun(migrateStreams);
+export const TriggerSuperChats = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 10 minutes").onRun(triggerSuperChats);
+export const MigrateSuperChats = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(TEMP_ANALYZE_TOPIC).onPublish(migrateSuperChats);
--- functions/src/migrateToBigQuery.ts
+++ functions/src/migrateToBigQuery.ts
@@ -1,16 +1,14 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import {ChangeType, migrateSuperChatsToBigQuery} from "./exportToBigQuery";
+import {ChangeType, migrateStreamsToBigQuery, migrateSuperChatsToBigQuery} from "./exportToBigQuery";
 import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
+import {Message} from "firebase-functions/lib/providers/pubsub";
+import {PubSub} from "@google-cloud/pubsub";
+import {TEMP_ANALYZE_TOPIC} from ".";
 
-export const migrateToBigQuery = async () => {
-  await migrateSuperChats();
-};
-
-
-const migrateSuperChats = async () => {
+export const migrateStreams = async () => {
   const db = admin.firestore();
-  const channels = await db.collection("channels").where("category", "in", ["hololive"]).get().catch((err) => {
+  const channels = await db.collection("channels").get().catch((err) => {
     functions.logger.error(err.message + "\n" + err.stack);
   });
 
@@ -38,23 +36,72 @@ const migrateSuperChats = async () => {
       tempStreams.push(stream);
     });
 
+    functions.logger.info("migrate channel " + channel.id + " videos: " + tempStreams.length);
+    await migrateStreamsToBigQuery(channel, tempStreams, ChangeType.CREATE);
+  }
+};
+
+export const triggerSuperChats = async () => {
+  const db = admin.firestore();
+  const channels = await db.collection("channels").where("category", "in", ["hololive", "nijisanji"]).get().catch((err) => {
+    functions.logger.error(err.message + "\n" + err.stack);
+  });
+
+  if (!channels || channels && channels.empty) {
+    return;
+  }
+
+  const tempChannels: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
+
+  channels.forEach((channel) => {
+    tempChannels.push(channel);
+  });
+
+  const pubsub = new PubSub({projectId: process.env.GCP_PROJECT});
+  const topic = await pubsub.topic(TEMP_ANALYZE_TOPIC);
+
+  for await (const channel of tempChannels) {
+    const streams = await channel.ref.collection("streams").get().catch((err) => {
+      functions.logger.error(err.message + "\n" + err.stack);
+    });
+    if (!streams || streams && streams.empty) {
+      continue;
+    }
+    let count = 0;
+    const tempStreams: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
+    streams.forEach((stream) => tempStreams.push(stream));
     for await (const stream of tempStreams) {
-      const superChats = await stream.ref.collection("superChats").get().catch((err) => {
-        functions.logger.error(err.message + "\n" + err.stack);
-      });
-
-      if (!superChats || superChats && superChats.empty) {
-        continue;
+      if (count < 1) {
+        await topic.publish(Buffer.from(JSON.stringify({channelId: channel.id, videoId: stream.id})));
+        count += 1;
       }
-
-      const tempSuperChats: DocumentSnapshot[] = [];
-      superChats.forEach((sc) => {
-        tempSuperChats.push(sc);
-      });
-      functions.logger.info("migrate video /" + channel.id + "/streams/" + stream.id + " sc: " + tempStreams.length);
-      await migrateSuperChatsToBigQuery(tempSuperChats, channel.id, stream.id, ChangeType.CREATE);
-      break;
     }
     break;
   }
+};
+
+export const migrateSuperChats = async (message: Message) => {
+  const params = messageToJSON(message);
+  const channelId = params.channelId;
+  const videoId = params.videoId;
+  const db = admin.firestore();
+  const superChats = await db.collection(`channels/${channelId}/streams/${videoId}/superChats`).get().catch((err) => {
+    functions.logger.error(err.message + "\n" + err.stack);
+  });
+
+  if (!superChats || superChats && superChats.empty) {
+    return;
+  }
+
+  const tempSuperChats: DocumentSnapshot[] = [];
+  superChats.forEach((sc) => {
+    tempSuperChats.push(sc);
+  });
+  functions.logger.info("migrate video /" + channelId + "/streams/" + videoId + " sc: " + superChats.size);
+  await migrateSuperChatsToBigQuery(tempSuperChats, channelId, videoId, ChangeType.CREATE);
+};
+
+const messageToJSON = (message: Message) => {
+  const jsonstr = Buffer.from(message.data, "base64").toString("utf-8");
+  return JSON.parse(jsonstr);
 };
