ad5a5fdb66850ba4f127252a1b6a5a6ee46d6668
natsumesou <natsumesouxx@gmail.com>
2021-06-16 23:44:30 +0900

    check

--- functions/src/migrateToBigQuery.ts
+++ functions/src/migrateToBigQuery.ts
@@ -1,9 +1,14 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import {ChangeType, migrateStreamsToBigQuery} from "./exportToBigQuery";
+import {ChangeType, migrateStreamsToBigQuery, migrateSuperChatsToBigQuery} from "./exportToBigQuery";
 import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
 
 export const migrateToBigQuery = async () => {
+  // await migrateStreams();
+  await migrateSuperChats();
+};
+
+const migrateStreams = async () => {
   const db = admin.firestore();
   const channels = await db.collection("channels").where("category", "==", "hololive").get().catch((err) => {
     functions.logger.error(err.message + "\n" + err.stack);
@@ -19,7 +24,7 @@ export const migrateToBigQuery = async () => {
     tempChannels.push(channel);
   });
 
-  for (const channel of tempChannels) {
+  for await (const channel of tempChannels) {
     const streams = await channel.ref.collection("streams").get().catch((err) => {
       functions.logger.error(err.message + "\n" + err.stack);
     });
@@ -39,6 +44,65 @@ export const migrateToBigQuery = async () => {
 
     functions.logger.info("migrate channel videos: " + tempStreams.length);
     await migrateStreamsToBigQuery(channel, tempStreams, ChangeType.CREATE);
+    break;
+  }
+};
+
+const migrateSuperChats = async () => {
+  const db = admin.firestore();
+  const channels = await db.collection("channels").where("category", "==", "hololive").get().catch((err) => {
+    functions.logger.error(err.message + "\n" + err.stack);
+  });
+
+  if (!channels || channels && channels.empty) {
+    return;
+  }
+
+  const tempChannels: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = [];
+
+  channels.forEach((channel) => {
+    tempChannels.push(channel);
+  });
+
+  for await (const channel of tempChannels) {
+    const streams = await channel.ref.collection("streams").get().catch((err) => {
+      functions.logger.error(err.message + "\n" + err.stack);
+    });
+
+    if (!streams || streams && streams.empty) {
+      continue;
+    }
+
+    const tempStreams: DocumentSnapshot[] = [];
+    let count = 0;
+    streams.forEach((stream) => {
+      if (count < 1) {
+        tempStreams.push(stream);
+        count += 1;
+      }
+    });
+
+    for await (const stream of tempStreams) {
+      const superChats = await stream.ref.collection("superChats").get().catch((err) => {
+        functions.logger.error(err.message + "\n" + err.stack);
+      });
+
+      if (!superChats || superChats && superChats.empty) {
+        continue;
+      }
+
+      const tempSuperChats: DocumentSnapshot[] = [];
+      let sCount = 0;
+      superChats.forEach((sc) => {
+        if (sCount < 3) {
+          tempSuperChats.push(sc);
+          sCount += 1;
+        }
+      });
+      functions.logger.info("migrate channel videos: " + tempStreams.length);
+      await migrateSuperChatsToBigQuery(tempSuperChats, channel.id, ChangeType.CREATE);
+      break;
+    }
     break;
   }
 };
