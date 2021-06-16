f4847a12d25f684a3ddec0ae4e323cf80c4c006a
natsumesou <natsumesouxx@gmail.com>
2021-06-17 07:32:52 +0900

    test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import {publishCreatorsWatch} from "./publishCreatorsWatch";
 import {analyzeChats} from "./analyzeChats";
 import {updateArchives} from "./updateArchives";
 import {RuntimeOptions} from "firebase-functions";
-import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
+// import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
 import {migrateSuperChats, triggerSuperChats} from "./migrateToBigQuery";
 
 admin.initializeApp();
@@ -26,8 +26,8 @@ const strongRuntimeOpts: RuntimeOptions = {
 export const PublishCreatorsWatchFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 30 minutes").onRun(publishCreatorsWatch);
 export const UpdateArchivesFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.topic(WATCH_TOPIC).onPublish(updateArchives);
 export const AnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(ANALYZE_TOPIC).onPublish(analyzeChats);
-export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
-export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
+// export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
+// export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
 
 // export const MigrateStreams = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.schedule("every 10 minutes").onRun(migrateStreams);
 export const TriggerSuperChats = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.schedule("every 10 minutes").onRun(triggerSuperChats);
