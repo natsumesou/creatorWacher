25cfff4b3dc62423422dec02da351f1b661e9452
natsumesou <natsumesouxx@gmail.com>
2021-06-16 22:47:28 +0900

    add migration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,8 @@ import {analyzeChats} from "./analyzeChats";
 import {updateArchives} from "./updateArchives";
 import {RuntimeOptions} from "firebase-functions";
 import {fetchSuperChats, tempAnalyzeChat} from "./fetchSuperChats";
-import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
+// import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
+import {migrateToBigQuery} from "./migrateToBigQuery";
 
 admin.initializeApp();
 
@@ -26,8 +27,9 @@ const strongRuntimeOpts: RuntimeOptions = {
 export const PublishCreatorsWatchFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 30 minutes").onRun(publishCreatorsWatch);
 export const UpdateArchivesFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.topic(WATCH_TOPIC).onPublish(updateArchives);
 export const AnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(ANALYZE_TOPIC).onPublish(analyzeChats);
-export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
-export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
+// export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
+// export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
+export const MigrateToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 5 minutes").onRun(migrateToBigQuery);
 
 export const FetchSuperChats = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 8 minutes").onRun(fetchSuperChats);
 export const TempAnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(TEMP_ANALYZE_TOPIC).onPublish(tempAnalyzeChat);
