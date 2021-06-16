5f7cc5f4f85f47dc47102f6003f43ffb82b5679c
natsumesou <natsumesouxx@gmail.com>
2021-06-16 20:00:51 +0900

    修正とsuperChatに対応

--- functions/src/exportToBigQuery.ts
+++ functions/src/exportToBigQuery.ts
@@ -19,7 +19,7 @@ export const exportStreamsToBigQuery = async (change: Change<DocumentSnapshot>, 
 
   if (changeType === ChangeType.DELETE) {
     const query = `DELETE \`${projectId}.${dataset}.${table}\` WHERE documentId = "${documentId}"`;
-    await exec(bigQuery, query);
+    return await exec(bigQuery, query);
   }
 
   const channel = await change.after.ref.parent.parent?.get();
@@ -28,13 +28,13 @@ export const exportStreamsToBigQuery = async (change: Change<DocumentSnapshot>, 
   }
 
   if (changeType === ChangeType.CREATE) {
-    const query = `INSERT \`${projectId}.${dataset}.${table}\` (chatAvailable, chatCount, chatDisabled, gameTitle, publishedAt, streamLengthSec, subscribeCount, superChatAmount, title, viewCount, startedAt, createdAt, superChatCount, id, channelTitle, category, channelId, documentId) VALUES ("${change.after.get("chatAvailable")}", ${change.after.get("chatCount")}, ${change.after.get("chatDisabled")}, "${change.after.get("gameTitle").replace(/"/g, "\\\"")}", TIMESTAMP("${change.after.get("publishedAt").getDate().toISOString()}"), ${change.after.get("streamLengthSec")}, ${change.after.get("subscribeCount")}, ${change.after.get("superChatAmount")}, "${change.after.get("title").replace(/"/g, "\\\"")}", ${change.after.get("viewCount")}, TIMESTAMP("${change.after.get("startedAt").toDate().toISOString()}") AND updatedAt = TIMESTAMP("${change.after.get("updatedAt").toDate().toISOString()}"), TIMESTAMP("${change.after.get("createdAt").toDate().toISOString()}"), ${change.after.get("superChatCount")}, "${change.after.id}", "${channel.get("title").replace(/"/g, "\\\"")}", "${channel.get("category")}", "${channel.id}", "${documentId}") WHERE documentId = "${documentId}"`;
-    await exec(bigQuery, query);
+    const query = `INSERT \`${projectId}.${dataset}.${table}\` (chatAvailable, chatCount, chatDisabled, gameTitle, publishedAt, streamLengthSec, subscribeCount, superChatAmount, title, viewCount, startedAt, createdAt, superChatCount, id, channelTitle, category, channelId, documentId) VALUES ("${change.after.get("chatAvailable")}", ${change.after.get("chatCount")}, ${change.after.get("chatDisabled")}, "${change.after.get("gameTitle").replace(/"/g, "\\\"")}", TIMESTAMP("${change.after.get("publishedAt").getDate().toISOString()}"), ${change.after.get("streamLengthSec")}, ${change.after.get("subscribeCount")}, ${change.after.get("superChatAmount")}, "${change.after.get("title").replace(/"/g, "\\\"")}", ${change.after.get("viewCount")}, TIMESTAMP("${change.after.get("startedAt").toDate().toISOString()}") AND updatedAt = TIMESTAMP("${change.after.get("updatedAt").toDate().toISOString()}"), TIMESTAMP("${change.after.get("createdAt").toDate().toISOString()}"), ${change.after.get("superChatCount")}, "${change.after.id}", "${channel.get("title").replace(/"/g, "\\\"")}", "${channel.get("category")}", "${channel.id}", "${documentId}")`;
+    return await exec(bigQuery, query);
   }
 
   if (changeType === ChangeType.UPDATE) {
     const query = `UPDATE \`${projectId}.${dataset}.${table}\` SET chatAvailable = "${change.after.get("chatAvailable")}" AND chatCount = ${change.after.get("chatCount")} AND chatDisabled = ${change.after.get("chatDisabled")} AND gameTitle = "${change.after.get("gameTitle").replace(/"/g, "\\\"")}" AND publishedAt = TIMESTAMP("${change.after.get("publishedAt").getDate().toISOString()}") AND streamLengthSec = ${change.after.get("streamLengthSec")} AND subscribeCount = ${change.after.get("subscribeCount")} AND superChatAmount = ${change.after.get("superChatAmount")} AND title = "${change.after.get("title").replace(/"/g, "\\\"")}" AND viewCount = ${change.after.get("viewCount")} AND startedAt = TIMESTAMP("${change.after.get("startedAt").toDate().toISOString()}") AND updatedAt = TIMESTAMP("${change.after.get("updatedAt").toDate().toISOString()}") AND createdAt = TIMESTAMP("${change.after.get("createdAt").toDate().toISOString()}") AND superChatCount = ${change.after.get("superChatCount")} AND id = "${change.after.id}" AND channelTitle = ${channel.get("title").replace(/"/g, "\\\"")}" AND category = "${channel.get("category")}" AND channelId = "${channel.id}" AND documentId = "${documentId}" WHERE documentId = "${documentId}"`;
-    await exec(bigQuery, query);
+    return await exec(bigQuery, query);
   }
 };
 
@@ -44,25 +44,27 @@ export const exportSuperChatsToBigQuery = async (change: Change<DocumentSnapshot
   const table = "superChats";
   const changeType = getChangeType(change);
   const documentId = getDocumentId(change);
+  const channelId = context.params.channelId;
+  const videoId = context.params.videoId;
 
   if (changeType === ChangeType.DELETE) {
     const query = `DELETE \`${projectId}.${dataset}.${table}\` WHERE documentId = "${documentId}"`;
-    await exec(bigQuery, query);
+    return await exec(bigQuery, query);
   }
 
-  const channel = await change.after.ref.parent.parent?.get();
+  const channel = await change.after.ref.parent.parent?.parent.parent?.get();
   if (!channel || !channel.exists) {
     return;
   }
 
   if (changeType === ChangeType.CREATE) {
-    const query = `${documentId}`;
-    await exec(bigQuery, query);
+    const query = `INSERT \`${projectId}.${dataset}.${table}\` (videoId, supporterChannelId, supporterDisplayName, amount, amountText, unit, thumbnail, paidAt, documentId, channelId) VALUES ("${videoId}", "${change.after.get("supporterChannelId")}", "${change.after.get("supporterDisplayName").replace(/"/g, "\\\"")}", ${change.after.get("amount")}, "${change.after.get("amountText")}", "${change.after.get("unit")}", "${change.after.get("thumbnail")}", TIMESTAMP("${change.after.get("paidAt").toDate().toISOString()}"), "${documentId}", "${channelId}")`;
+    return await exec(bigQuery, query);
   }
 
   if (changeType === ChangeType.UPDATE) {
-    const query = `${documentId}`;
-    await exec(bigQuery, query);
+    const query = `UPDATE \`${projectId}.${dataset}.${table}\` SET videoId = "${videoId}" AND supporterChannelId = "${change.after.get("supporterChannelId")}" AND supporterDisplayName = ${change.after.get("supporterDisplayName").replace(/"/g, "\\\"")} AND amount = ${change.after.get("amount")} AND amountText = "${change.after.get("amountText")}" AND unit = "${change.after.get("unit")}" AND thumbnail = "${change.after.get("thumbnail")}" AND paidAt = TIMESTAMP("${change.after.get("paidAt").toDate().toISOString()}") AND documentId = "${documentId}" AND channelId = "${channelId}" WHERE documentId = "${documentId}"`;
+    return await exec(bigQuery, query);
   }
 };
 
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ import {analyzeChats} from "./analyzeChats";
 import {updateArchives} from "./updateArchives";
 import {RuntimeOptions} from "firebase-functions";
 import {fetchSuperChats, tempAnalyzeChat} from "./fetchSuperChats";
-import {exportStreamsToBigQuery} from "./exportToBigQuery";
+import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
 
 admin.initializeApp();
 
@@ -26,7 +26,8 @@ const strongRuntimeOpts: RuntimeOptions = {
 export const PublishCreatorsWatchFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 30 minutes").onRun(publishCreatorsWatch);
 export const UpdateArchivesFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.topic(WATCH_TOPIC).onPublish(updateArchives);
 export const AnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(ANALYZE_TOPIC).onPublish(analyzeChats);
-export const ExportToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
+export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
+export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
 
 export const FetchSuperChats = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 8 minutes").onRun(fetchSuperChats);
 export const TempAnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(TEMP_ANALYZE_TOPIC).onPublish(tempAnalyzeChat);
