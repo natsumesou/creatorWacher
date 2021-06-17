850dd7627fefdaa48253f3fa1cd14d41b731a3d7
natsumesou <natsumesouxx@gmail.com>
2021-06-17 09:52:24 +0900

    通常運転復活

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,7 @@ import {publishCreatorsWatch} from "./publishCreatorsWatch";
 import {analyzeChats} from "./analyzeChats";
 import {updateArchives} from "./updateArchives";
 import {RuntimeOptions} from "firebase-functions";
-// import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
-import {migrateStreams, migrateSuperChats, triggerSuperChats} from "./migrateToBigQuery";
+import {exportStreamsToBigQuery, exportSuperChatsToBigQuery} from "./exportToBigQuery";
 
 admin.initializeApp();
 
@@ -26,9 +25,5 @@ const strongRuntimeOpts: RuntimeOptions = {
 export const PublishCreatorsWatchFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.schedule("every 30 minutes").onRun(publishCreatorsWatch);
 export const UpdateArchivesFunction = functions.runWith(weakRuntimeOpts).region(REGION).pubsub.topic(WATCH_TOPIC).onPublish(updateArchives);
 export const AnalyzeChatsFunction = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(ANALYZE_TOPIC).onPublish(analyzeChats);
-// export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
-// export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
-
-export const MigrateStreams = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.schedule("every 10 minutes").onRun(migrateStreams);
-export const TriggerSuperChats = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.schedule("every 10 minutes").onRun(triggerSuperChats);
-export const MigrateSuperChats = functions.runWith(strongRuntimeOpts).region(REGION).pubsub.topic(TEMP_ANALYZE_TOPIC).onPublish(migrateSuperChats);
+export const ExportStreamsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}").onWrite(exportStreamsToBigQuery);
+export const ExportSuperChatsToBigQuery = functions.runWith(weakRuntimeOpts).region(REGION).firestore.document("channels/{channelId}/streams/{videoId}/superChats/{superChatId}").onWrite(exportSuperChatsToBigQuery);
